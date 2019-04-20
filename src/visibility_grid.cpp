786c16650d84986f21ccd13f8398138e8eb5d6af
Nicola Lissandrini <nicola.lissandrini.1@gmail.com>
2019-04-20 16:05:27 +0200

    temp commit

--- src/visibility_grid.cpp
+++ src/visibility_grid.cpp
@@ -8,38 +8,63 @@ using namespace std;
 
 #define CELL_SIZE 0.1
 #define EDGE_SIZE 0.0008
-#define CSH (CELL_SIZE/2)
-#define ESH (EDGE_SIZE/2)
+#define CSH (cellSize/2)
+#define ESH (edgeSize/2)
+#define HEIGHT ESH
 
 void VisibilityGrid::Load (ModelPtr _model, sdf::ElementPtr _sdf)
 {
-	Color colorInner(0,0,0,0.3), colorZero (0,0,0,0);
-	Color colorEdge (0,0,0,0.45);
-
 	model = _model;
 
 	node = transport::NodePtr (new transport::Node());
-	node->Init (model->GetWorld ()->Name ());
+	node->Init ();
 	visPub = node->Advertise<msgs::Visual> ("~/visual",1000);
 
+	cellVisual = new CellVisual (node, visPub);
 
-	buildPlane (colorInner, colorZero);
-	buildEdge1 (colorEdge, colorZero);
-	buildEdge2 (colorEdge, colorZero);
-
-	visPub->Publish (edge1);
-	visPub->Publish (edge2);
-	visPub->Publish (plane);
+	cellVisual->build (0, CELL_SIZE, EDGE_SIZE);
 
 	updateConnection = event::Events::ConnectRender (
 				boost::bind(&VisibilityGrid::UpdateChild, this));
 }
 
-void VisibilityGrid::buildPlane (const Color &ambientColor, const Color &emitColor)
-{
-	plane.set_name ("__CELL_PLANE__");
-	plane.set_parent_name (model->GetScopedName ());
 
+void VisibilityGrid::UpdateChild ()
+{
+	common::Time tt = model->GetWorld ()->SimTime ();
+	double t = tt.Double ();
+
+	cellVisual->updatePose (Pose3d (t*0.1, 0, 0, 0,0,0));
+	cellVisual->redraw ();
+}
+
+void CellVisual::build (int _id, double _cellSize, double _edgeSize)
+{
+	Color colorInner(0,0,0,0.3), colorZero (0,0,0,0);
+	Color colorEdge (0,0,0,0.45);
+
+	cellSize = _cellSize;
+	edgeSize = _edgeSize;
+
+	setId (_id);
+
+	buildPlane (colorInner, colorZero);
+	buildEdge1 (colorEdge, colorZero);
+	buildEdge2 (colorEdge, colorZero);
+}
+
+void CellVisual::setId(int _id)
+{
+	id = to_string (_id);
+
+	plane.set_name (string ("cell" + id + "_plane").c_str());
+	edge1.set_name (string ("cell" + id + "_edge1").c_str());
+	edge2.set_name (string ("cell" + id + "_edge2").c_str());
+}
+
+
+void CellVisual::buildPlane (const Color &ambientColor, const Color &emitColor)
+{
 	msgs::Geometry *geomMsg = plane.mutable_geometry ();
 	geomMsg->set_type (msgs::Geometry::PLANE);
 	geomMsg->mutable_plane ()->mutable_size ()->set_x (CELL_SIZE);
@@ -52,13 +77,11 @@ void VisibilityGrid::buildPlane (const Color &ambientColor, const Color &emitCol
 	msgs::Set (plane.mutable_material ()->mutable_diffuse (), ambientColor);
 	msgs::Set (plane.mutable_material ()->mutable_emissive (), emitColor);
 
-	msgs::Set (plane.mutable_pose (), Pose3d (0,0,EDGE_SIZE/2, 0,0,0));
+	msgs::Set (plane.mutable_pose (), Pose3d (0,0,HEIGHT, 0,0,0));
 }
 
-void VisibilityGrid::buildEdge1 (const Color &ambientColor, const Color &emitColor)
+void CellVisual::buildEdge1 (const Color &ambientColor, const Color &emitColor)
 {
-	edge1.set_name ("__CELL_EDGE_1__");
-	edge1.set_parent_name (model->GetScopedName ());
 	msgs::Geometry *edgesGeom = edge1.mutable_geometry ();
 
 	edgesGeom->set_type (msgs::Geometry::POLYLINE);
@@ -80,14 +103,12 @@ void VisibilityGrid::buildEdge1 (const Color &ambientColor, const Color &emitCol
 	msgs::Set (edge1.mutable_material ()->mutable_emissive (), emitColor);
 }
 
-void VisibilityGrid::buildEdge2 (const Color &ambientColor, const Color &emitColor)
+void CellVisual::buildEdge2 (const Color &ambientColor, const Color &emitColor)
 {
-	edge2.set_name ("__CELL_EDGE_2__");
-	edge2.set_parent_name (model->GetScopedName ());
 	msgs::Geometry *edgesGeom = edge2.mutable_geometry ();
 
 	edgesGeom->set_type (msgs::Geometry::POLYLINE);
-	msgs::Polyline *poly  = edgesGeom->add_polyline ();
+	msgs::Polyline *poly = edgesGeom->add_polyline ();
 
 	msgs::Set (poly->add_point (), Vector2d (-CSH-ESH,-CSH-ESH));
 	msgs::Set (poly->add_point (), Vector2d (-CSH-ESH,+CSH+ESH));
@@ -104,12 +125,84 @@ void VisibilityGrid::buildEdge2 (const Color &ambientColor, const Color &emitCol
 	msgs::Set (edge2.mutable_material ()->mutable_emissive (), emitColor);
 }
 
-void VisibilityGrid::UpdateChild ()
+void CellVisual::redraw ()
 {
-	common::Time tt = model->GetWorld ()->SimTime ();
-	double t = tt.Double ();
+	visPub->Publish (plane);
+	visPub->Publish (edge1);
+	visPub->Publish (edge2);
+}
 
-	model->SetWorldPose (Pose3d (t*0.1,0,0,0,0,0));
+void CellVisual::updatePose (const Pose3d &pose)
+{
+	msgs::Set (plane.mutable_pose (), pose * Pose3d (0,0,HEIGHT, 0,0,0));
+	msgs::Set (edge1.mutable_pose (), pose * Pose3d (0,0,HEIGHT, 0,0,0));
+	msgs::Set (edge2.mutable_pose (), pose * Pose3d (0,0,HEIGHT, 0,0,0));
+}
+
+Vector3d CellMatrix::cellMap (int i, int j) {
+	return Vector3d (xM + double (i) * cellSize, yM + double (j) * cellSize, 0);
+}
+
+void CellMatrix::build (double _cellSize, const string &parentName)
+{
+	cells.resize (rows * cols);
+
+	cellSize = _cellSize;
+
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < cols; j++) {
+			CellVisual *curr = new CellVisual (node, visPub);
+			Vector3d currPos = cellMap (i, j);
+
+			curr->build (cols*i + j, cellSize, EDGE_SIZE);
+			curr->updatePose (Pose3d (currPos, Quaterniond::Identity));
+			curr->redraw ();
+		}
+	}
+
+
+
 }
 
 GZ_REGISTER_MODEL_PLUGIN(VisibilityGrid)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/visibility_grid.h
+++ src/visibility_grid.h
@@ -4,6 +4,7 @@
 #include <gazebo/gazebo.hh>
 #include <gazebo/physics/physics.hh>
 #include <gazebo/physics/Model.hh>
+#include <gazebo/rendering/Visual.hh>
 
 #include <ros/ros.h>
 #include <ros/callback_queue.h>
@@ -12,7 +13,62 @@
 
 namespace gazebo {
 
-namespace rendering {
+
+class CellVisual
+{
+	transport::NodePtr node;
+	transport::PublisherPtr visPub;
+	msgs::Visual plane, edge1, edge2;
+
+	double cellSize;
+	double edgeSize;
+	std::string id;
+	void setId (int _id);
+
+	void buildPlane (const ignition::math::Color &ambientColor, const ignition::math::Color &emitColor);
+	void buildEdge1 (const ignition::math::Color &ambientColor, const ignition::math::Color &emitColor);
+	void buildEdge2 (const ignition::math::Color &ambientColor, const ignition::math::Color &emitColor);
+
+
+public:
+	CellVisual (transport::NodePtr _node, transport::PublisherPtr _visPub):
+		node(_node),
+		visPub(_visPub)
+	{}
+
+	void build (int _id, double _cellSize, double _edgeSize);
+	void updatePose (const ignition::math::Pose3d &pose);
+	void redraw ();
+};
+
+class CellMatrix
+{
+	transport::NodePtr node;
+	transport::PublisherPtr visPub;
+	std::vector<CellVisual*> cells;
+	int rows, cols;
+	double xM, yM, cellSize;
+
+	ignition::math::Vector3d cellMap (int i, int j);
+
+public:
+	CellMatrix (int _rows, int _cols, double _xM, double _yM,
+				transport::NodePtr _node, transport::PublisherPtr _visPub):
+		node(_node),
+		visPub(_visPub),
+		cells(_rows*_cols),
+		rows(_rows),
+		cols(_cols),
+		xM(_xM), yM(_yM)
+	{}
+
+	void build (double cellSize, const std::string &parentName);
+
+	CellVisual &operator() (int i, int j) {
+		assert (cols*i + j < cells.size ());
+		return *cells[cols*i + j];
+	}
+};
 
 class VisibilityGrid : public ModelPlugin
 {
@@ -22,13 +78,12 @@ class VisibilityGrid : public ModelPlugin
 	ros::CallbackQueue rosQueue;
 	ros::Subscriber rosSub;
 	std::thread rosQueueThread;
-	msgs::Visual plane, edge1, edge2;
 	physics::ModelPtr model;
 	event::ConnectionPtr updateConnection;
 
-	void buildPlane (const ignition::math::Color &ambientColor, const ignition::math::Color &emitColor);
-	void buildEdge1 (const ignition::math::Color &ambientColor, const ignition::math::Color &emitColor);
-	void buildEdge2 (const ignition::math::Color &ambientColor, const ignition::math::Color &emitColor);
+	CellVisual *cellVisual;
+
+	void publishAll ();
 
 protected:
 	virtual void UpdateChild ();
@@ -40,8 +95,6 @@ public:
 
 	void Load (physics::ModelPtr _model, sdf::ElementPtr _sdf);
 };
-
-}
 
 }
 #endif // VISIBILITY_GRID_H
