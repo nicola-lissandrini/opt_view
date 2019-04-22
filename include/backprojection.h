a79150bf25df4c80eec06e64feb0b5d8b18e3666
Nicola Lissandrini <nicola.lissandrini.1@gmail.com>
2019-04-22 23:12:19 +0200

    backprojection progress

--- include/backprojection.h
+++ include/backprojection.h
@@ -2,18 +2,42 @@
 #define BACKPROJECTION_H
 
 #include <Eigen/Geometry>
+#include <Eigen/Dense>
 #include <eigen_conversions/eigen_msg.h>
 #include <sensor_msgs/CameraInfo.h>
+#include <nav_msgs/Odometry.h>
 #include <opt_view/ProjectedView.h>
+
+#include <Eigen/SVD>
 
 #define NODE_NAME "backprojection"
 #include "pd_rosnode.h"
 #include "common.h"
 
+#define POINTS_NO 5
+
+#define pinv(A) ((A).completeOrthogonalDecomposition().pseudoInverse())
+
 class BackProjection
 {
+	Eigen::Matrix<double, 3, 4>  projectionMatrix;
+	int pxWidth, pxHeight;
+	std::vector<Eigen::Vector2d> pointsToProject;
+	Eigen::Isometry3d pose;
+
+	bool initialized;
+	void updatePointsToProject (double width, double height);
+	Eigen::Vector3d backProject (Eigen::Vector2d imagePoint);
+
 public:
-	BackProjection () {}
+	BackProjection ():
+		initialized(false),
+		pointsToProject(POINTS_NO)
+	{}
+
+	void updateParams (const sensor_msgs::CameraInfo &cameraInfo);
+	void updatePose (const Eigen::Isometry3d &newPose);
+	std::vector<Eigen::Vector3d> backProjectPoints ();
 };
 
 class BackProjectionNode : public PdRosNode
@@ -30,8 +54,10 @@ public:
 	BackProjectionNode ();
 
 	void cameraInfoCallback (const sensor_msgs::CameraInfo &cameraInfo);
+	void cameraOdomCallback (const nav_msgs::Odometry &odom);
 };
 
 
 
 #endif // BACKPROJECTION_H
+
--- src/backprojection_node.cpp
+++ src/backprojection_node.cpp
@@ -18,44 +18,90 @@ void BackProjectionNode::initParams ()
 
 void BackProjectionNode::initROS ()
 {
-	string cameraTopic = paramString (params["topic_pre"])
-			+ paramString (params["agent_id"])
-			+ paramString (params["camera_info_post"]);
-	string projectedTopic =  paramString (params["topic_pre"])
-			+ paramString (params["agent_id"])
-			+ paramString (params["projected_view_post"]);
+	string cameraTopic = paramString (params["camera_info_topic"]);
+	string odomTopic = paramString (params["camera_odom_topic"]);
+	string projectedTopic =  paramString (params["projected_view_topic"]);
 
 
 	cameraInfoSub = nh.subscribe (cameraTopic, 1, &BackProjectionNode::cameraInfoCallback, this);
+	cameraInfoSub = nh.subscribe (odomTopic, 1, &BackProjectionNode::cameraOdomCallback, this);
 	projectedPointsPub = nh.advertise<opt_view::ProjectedView> (projectedTopic, 1);
 }
 
-void BackProjectionNode::cameraInfoCallback (const sensor_msgs::CameraInfo &cameraInfo)
-{
+void BackProjection::updatePose(const Isometry3d &newPose) {
+	pose = newPose;
+}
+
+void BackProjectionNode::cameraInfoCallback (const sensor_msgs::CameraInfo &cameraInfo) {
+	backProjection.updateParams (cameraInfo);
+}
+
+void BackProjectionNode::cameraOdomCallback (const nav_msgs::Odometry &odom) {
+	Isometry3d eigenPose;
+
+	tf::poseMsgToEigen (odom.pose.pose, eigenPose);
+	backProjection.updatePose (eigenPose);
 }
 
 int BackProjectionNode::actions ()
 {
 	opt_view::ProjectedView projView;
+	vector<Vector3d> points;
+	projView.points.resize (points.size ());
 
-	temp.points.resize (4);
+	points = backProjection.backProjectPoints ();
 
-	temp.points[0].x = 0;
-	temp.points[0].y = 0;
-	temp.points[0].z = -2;
-	temp.points[1].x = 1;
-	temp.points[1].y = 0;
-	temp.points[1].z = -2;
-	temp.points[2].x = 1;
-	temp.points[2].y = 1;
-	temp.points[2].z = -2;
-	temp.points[3].x = 0;
-	temp.points[3].y = 1;
-	temp.points[3].z = -2;
+	for (int i = 0; i < points.size (); i++) {
+		projView.points[i].x = points[i](0);
+		projView.points[i].y = points[i](1);
+		projView.points[i].z = points[i](2);
+	}
 
-	projectedPointsPub.publish (temp);
+	projectedPointsPub.publish (projView);
 
 	return 0;
+}
+
+Vector3d BackProjection::backProject (Vector2d imagePoint)
+{
+	Vector3d generic, director;
+	Vector3d homogeneousImagePoint = Vector3d (imagePoint(0), imagePoint(1), 1);
+
+	generic = pinv (projectionMatrix) * homogeneousImagePoint;
+	director = generic * 1 / generic.norm ();
+}
+
+
+std::vector<Vector3d> BackProjection::backProjectPoints()
+{
+	std::vector<Vector3d> points;
+
+	points.resize (POINTS_NO);
+	for (int i = 0; i < POINTS_NO; i++)
+		points[i] = backProject (pointsToProject[i]);
+
+	return points;
+}
+
+void BackProjection::updateParams (const sensor_msgs::CameraInfo &cameraInfo)
+{
+	initialized = true;
+	for (int i = 0; i < projectionMatrix.rows (); i++)
+		for (int j = 0; j < projectionMatrix.cols (); i++)
+			projectionMatrix(i,j) = cameraInfo.P[i * projectionMatrix.cols () + j];
+	pxWidth = cameraInfo.width;
+	pxHeight = cameraInfo.height;
+
+	updatePointsToProject (pxWidth, pxHeight);
+}
+
+void BackProjection::updatePointsToProject (double width, double height)
+{
+	pointsToProject[0] = Vector2d (0, 0);
+	pointsToProject[1] = Vector2d (width, 0);
+	pointsToProject[2] = Vector2d (0, height);
+	pointsToProject[3] = Vector2d (width, height);
+	pointsToProject[4] = Vector2d (width/2, height/2);
 }
 
 int main (int argc, char *argv[])
@@ -65,3 +111,28 @@ int main (int argc, char *argv[])
 
 	return bpn.spin ();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
